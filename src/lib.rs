c6c07990120065cb12de7b47a93adfd705e35acd
Valenciano118 <borja33357@gmail.com>
2022-05-06 19:47:23 +0200

    Added TODO on lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use rand::{Rng,thread_rng};
 
-pub fn generate_random_numbers(vector: &mut Vec<u128>) {
+//TODO:
+//Remove the pub keyword since this function should be private, it's public right now for testing purposes only 
+pub fn generate_random_numbers(vector: &mut Vec<u128>) { 
     for i in 0..vector.len(){
         vector[i] = thread_rng().gen();
     }
