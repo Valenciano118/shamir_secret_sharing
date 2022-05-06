aa110d8b0e3d16c75a688cee9b2d89e3f030958a
Valenciano118 <borja33357@gmail.com>
2022-05-06 16:00:48 +0200

    Leer de teclado

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Introduce el secreto: ");
+    let mut secret = String::new();
+
+    io::stdin()
+        .read_line(&mut secret)
+        .expect("Fallo al leer la linea");
+    
+    println!("El secreto introducido es:{}",secret);
 }
