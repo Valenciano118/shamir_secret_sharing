b9330de5e3b6219d058d567e4f335d66d9951147
Valenciano118 <borja33357@gmail.com>
2022-05-06 16:02:21 +0200

    Changed to english

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::io;
 fn main() {
-    println!("Introduce el secreto: ");
+    println!("Input you secret: ");
     let mut secret = String::new();
 
     io::stdin()
         .read_line(&mut secret)
-        .expect("Fallo al leer la linea");
+        .expect("Failed to read the line");
     
-    println!("El secreto introducido es:{}",secret);
+    println!("The secret introduced is:{}",secret);
 }
